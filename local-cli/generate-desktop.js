27c1218b885efc14e7a99ce02637520bf9dfbdae
Volodymyr Kozieiev <vkjr.sp@gmail.com>
2020-04-03 19:23:44 +0300

    fix

--- local-cli/generate-desktop.js
+++ local-cli/generate-desktop.js
@@ -2,7 +2,6 @@
 
 const fs = require('fs');
 const path = require('path');
-const chalk = require('chalk');
 const copyProjectTemplateAndReplace = require('./generator-desktop').copyProjectTemplateAndReplace;
 
 /**
