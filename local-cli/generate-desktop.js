a5666d782ecbf060dd2ca7c41bc7e020c19c1ab7
Volodymyr Kozieiev <vkjr.sp@gmail.com>
2020-04-03 14:13:20 +0300

    Placeholders for files generation

--- local-cli/generate-desktop.js
+++ local-cli/generate-desktop.js
@@ -1,9 +1,28 @@
 'use strict';
 
-const chalk = require('chalk');
+const fs = require('fs');
+const path = require('path');
+const copyProjectTemplateAndReplace = require('./generator-desktop').copyProjectTemplateAndReplace;
 
-module.exports = function(path, name) {
-  console.log(chalk.red("generate-desktop invoked"));
+/**
+ * Simple utility for running the Windows generator.
+ *
+ * @param  {String} projectDir root project directory (i.e. contains index.js)
+ * @param  {String} name       name of the root JS module for this app
+ */
+function generateDesktop (projectDir, name) {
+
+  console.log(chalk.white.bold(`Creating ${projectDir}`));
+
+  if (!fs.existsSync(projectDir)) {
+    fs.mkdirSync(projectDir);
+  }
+  copyProjectTemplateAndReplace(
+    path.join(__dirname, 'generator-desktop', 'templates'),
+    projectDir,
+    name
+  );
 }
 
+module.exports = generateDesktop;
 
