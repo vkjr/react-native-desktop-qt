5c0a14df208777de01e04f561a2fbfc2222e6892
Volodymyr Kozieiev <vkjr.sp@gmail.com>
2020-04-06 18:13:41 +0300

    Desktop generation fixes

--- local-cli/generator-desktop/templates/main.cpp
+++ local-cli/generator-desktop/templates/main.cpp
@@ -135,7 +135,7 @@ private:
     QString m_packagerHost = "localhost";
     QString m_packagerPort = "8081";
     QString m_localSource;
-    QString m_packagerTemplate = "http://%1:%2/index.desktop.bundle?platform=desktop&dev=true";
+    QString m_packagerTemplate = "http://%1:%2/index.bundle?platform=desktop-qt&dev=true";
     QUrl m_codeLocation;
     QString m_pluginsPath;
     QString m_executor = "RemoteServerConnection";
